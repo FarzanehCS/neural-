d8f59f045486d09ef3ff3ce4a7487f0e4190c082
Farzaneh <farzaneh.tabandeh@mail.utoronto.ca>
2018-08-06 15:48:57 -0400

    good version

--- neural-networks-and-deep-learning/README.md
+++ neural-networks-and-deep-learning/README.md
@@ -1,2 +1,51 @@
 # A simple sample for "Neural Networks and Deep Learning"
 
+This is a sample implementaion of the "Neural Networks and Deep Learning".
+The Neural-networks-and-deep-learning package is the very basic implementation and the
+Neural_Network_Scikit package is the Neural Networks and Deep Learning implementation using scikit_learn and
+scipy libraries.
+
+
+## Getting Started
+
+
+### Prerequisites
+numpy
+scikit-learn
+scipy
+
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+
+## Acknowledgments
+
+Special Thanks to:
+*
+* etc
--- neural-networks-and-deep-learning/src/my_network.py
+++ neural-networks-and-deep-learning/src/my_network.py
@@ -1,65 +1,60 @@
 import math
 import random
 import numpy as np
+import neural_data
 
 
+# helper functions
 def sigma(x):
     return 1 / (1 + np.exp(-x))
 
 
-def reader_hourly(line, file_write):
-    splitted = line.split()
-    open_price = splitted[2]
-    high_price = splitted[3]
-    low_price = splitted[4]
-    close_price = splitted[5]
+# class network
 
-    if open_price <= close_price:
-        result = (round(sigma(float(open_price)), 5), "buy")
-    else:
-        result = (round(sigma(float(open_price)), 5), "sell")
+class Network:
+    """
+    A neural Network.
 
-    file_write.write(str(result[0]) + " " + str(result[1]) + "\n")
-    # file_write.write(str(result[1]) + "\n")
+    Neural Network takes some numbers as data for every traing exmaple and learn the best parametsr for it
+    by gradiant decent and back_propagation and eventullay comes up with best weights and biaaess for
+    the nurons in every layer and can then predict labl for the future data input.
 
-    return result
+    Parameters
+    ----------
+    error_of_calculation : float
+       error of diffrence between the desired output and the resultant output each time the learning procedure
+       takes place. This indictes when the learning should stop and take the resultant weights and biases
+       as the end reasults that nn classifier can use from then on.
+    learning_rate : float
+       Rate on which the cost function calculates next weights and biases in gradiant decent.
+    num_of_layers : integer
+       Number of layers that Network should have.
+    list_of_nurons_per_layer: List of integers
+        List of number of nurons that every layer in Network should have.
+    data_file_name : String
+        Name of the file containg the data to be used for network learing process.
 
-
-def reader_daily(line, write_to_file):
-    # print("line is" + line + "\n")
-    splitted = line.split()
-    # print(splitted)
-    open_price = splitted[1]
-    high_price = splitted[2]
-    low_price = splitted[3]
-    close_price = splitted[4]
-    # print(open_price)
-    # print(close_price)
-
-    if open_price <= close_price:
-        result = (round(sigma(float(open_price)), 5), "buy")
-    else:
-        result = (round(sigma(float(open_price)), 5), "sell")
-
-        write_to_file.write("OpenDaily" + " " + str(result[0]) + " " + str(result[1]) + "\n")
-        # write_to_file.write(str(result[1]) + "\n")
-    return result
+    first_layer_num_of_nurons: integer
+        number of nurons in the first layer.
 
 
-# Class network
+    Examples
+    --------
+    To define the a neural network with 24 as initial number of nurons in first layer and other attribuites as
+    described above, we can instantiate it as :
 
-class network:
-    file_read_daily = None
-    file_read_hourly = None
-    open_file_write = None
-    open_file_data_all = None
+    >>> my_network = Network(0.001,0.01, 4, [10, 10, 10, 2], "file.csv",24)
+    """
 
-    def __init__(self, error_of_calculation, num_of_layers, list_of_nurons_per_layer, data_file_list, x):
+    open_file_data = None
 
-        self.alpha = 0.01
+    def __init__(self, error_of_calculation, learning_rate, num_of_layers, list_of_nurons_per_layer, data_file_name,
+                 first_layer_num_of_nurons):
+
+        self.alpha = learning_rate
         # self.set_up(data_file_list)
         # self.provide_data(network.file_read_daily, network.file_read_hourly, network.open_file_write)
-        self.initial_data_dim = x
+        self.initial_data_dim = first_layer_num_of_nurons
         self.error = error_of_calculation
         self.num_layers = num_of_layers
         self.num_nurons_lst = list_of_nurons_per_layer
@@ -70,58 +65,41 @@ class network:
         self.label = " "
         self.list_of_trained_w_and_b = {"w": [], "b": []}
         self.list_of_trained = {"w": [], "b": []}
-        self.data_file = data_file_list
-        self.set_up(data_file_list)
+        self.data_file = data_file_name
+        self.set_up()
         # self.provide_data(network.file_read_daily, network.file_read_hourly, network.open_file_write)
-        network.open_file_data_all = open(self.data_file[2], "r")
+        Network.open_file_data = open(self.data_file, "r")
 
-    def set_up(self, data_file_list):
+    def set_up(self):
 
-        network.file_read_daily = open(data_file_list[0])
-        network.file_read_hourly = open(data_file_list[1])
+        """
+        Sets up the neural network inclusing openning the data files and setting up the random weights and biases
+        for the network to start.
+        :return: None
+        :rtype: None
+        """
+
         # network.open_file_write = open(data_file_list[2], "w")
-
         pre = self.initial_data_dim
         for i in range(self.num_layers):
-            # print(self.num_layers)
             w = np.random.random((self.num_nurons_lst[i], pre))
             b = np.random.random((self.num_nurons_lst[i], 1))
             pre = self.num_nurons_lst[i]
             self.list_of_biases.append(b)
             self.list_of_weights.append(w)
 
-    def refresh(self):
+    #
+    # def refresh(self):
+    #
+    #     Network.file_read_daily.close()
+    #     Network.file_read_hourly.close()
+    #     Network.open_file_write.close()
+    #     Network.file_read_daily = open(self.data_file_list[0])
+    #     Network.file_read_hourly = open(self.data_file_list[1])
+    #     Network.open_file_write = open(self.data_file_list[2], "w")
 
-        network.file_read_daily.close()
-        network.file_read_hourly.close()
-        network.open_file_write.close()
-        network.file_read_daily = open(self.data_file_list[0])
-        network.file_read_hourly = open(self.data_file_list[1])
-        network.open_file_write = open(self.data_file_list[2], "w")
 
-    def provide_data(self, daily, hourly, open_file_write):
 
-        line = daily.readline()
-        line = hourly.readline()
-        # line = file_read_hourly.readline()
-        lst = []
-        # label = ""
-        while line != "":
-
-            for i in range(24):
-                line = hourly.readline()
-                if line != "":
-                    # print(line + "this is form hours")
-                    res = reader_hourly(line, open_file_write)
-                    lst.append(float(res[0]))
-            line = daily.readline()
-            # print("print of line daily is .........." + line + "\n")
-            if len(line) != 0:
-                res2 = reader_daily(line, open_file_write)
-        open_file_write.close()
-
-        # line = file_read_hourly.readline()
-        network.open_file_data_all = open(self.data_file[2], "r")
 
     def read_one_set_of_data(self, name_of_file):
 
@@ -160,7 +138,6 @@ class network:
 
     def main_trainer(self, X, pre_num_nurons, num_of_layers):
 
-        # y = np.random.random((self.initial_data_dim, 1))
         y = []
         X = np.array(X).reshape((24, 1))
         index = 0
@@ -181,14 +158,11 @@ class network:
     def forward_trained(self, X, y, index, layer, num_nurons):
 
         lst = []
-
-        # for i in range(num_nurons):
         w = self.list_of_trained["w"][layer]
         b = self.list_of_trained["b"][layer]
         y_inter = w.dot(X) + b
         y = sigma(y_inter)
         lst.append(y)
-        # y = np.array(y).reshape(num_nurons, 1)
         self.list_of_a_i.append(y)
         self.list_of_z_i.append(y_inter)
         return y
@@ -234,43 +208,31 @@ class network:
 
     def modify_w(self, W):
         index = 0
-        # index2 =0
         row = 0
         col = 0
         sum_m = 0
         n = 0
         i = 0
         t = self.num_layers
-        # print(W)
         d1 = W[0][index].shape[0]
         d2 = W[0][index].shape[1]
-        # print(d1)
-        # print(d2)
-
-        # some_w = np.array(W[0][index]).reshape((self.list_of_weights[index].shape[0], self.list_of_weights[index].shape[1]))
         some_w = np.array(W[0][index])
         while i < t:
-
             while row < d1:
-
                 while col < d2:
-
                     for lst in W:
                         sum_m = sum_m + lst[index][row, col]
-
                         n = n + 1
                     mean = sum_m / n
                     some_w[row, col] = mean
                     col = col + 1
                 row = row + 1
                 col = 0
-
                 index = index + 1
                 d1 = W[0][index].shape[0]
                 d2 = W[0][index].shape[1]
                 self.list_of_trained.get("w").append(some_w)
                 some_w = np.array(W[0][index])
-
             i = i + 1
 
     def modify_b(self, W):
@@ -318,23 +280,6 @@ class network:
         :return:
         :rtype: tuple of updated matrices of weights and biases.
         """
-        # lst_w = []
-        # lst_b = []
-        # new_w = 0
-        # new_b = 0
-        # for i in range(num_layers, -1, -1):
-        #     z = self.list_of_z_i[i]
-        #     ai = sigma(self.list_of_z_i[i])
-        #     ai_minus = sigma(self.list_of_z_i[i - 1])
-        #
-        #     delta_w = ai_minus * self.derivative(z) * self.cost_of_ai(i, y)
-        #     delta_b = self.derivative(z) * self.cost_of_ai(ai, y)
-        #     lst_w.append(delta_w)
-        #     lst_b.append(delta_b)
-        #     new_w = self.list_of_weights + (np.array(lst_w).reshape(self.list_of_weights[i].shape[0])).dot(self.alpha)
-        #     new_b = self.list_of_biases + np.array(lst_b).reshape(self.list_of_biases[i].shape[0]).dot(self.alpha)
-        #     self.list_of_weights[i] = new_w
-        #     self.list_of_biases[i] = new_b
 
         y = np.array([1, 0])
         if desired_y == 0:
@@ -342,9 +287,6 @@ class network:
         else:
             com_lst = [0, 1]
         p = -2
-        # z_i = self.list_of_z_i[p]
-        # a_i = sigma(self.list_of_z_i[p])
-        # a_t = sigma(self.list_of_z_i[p - 1])
         main_index = 0
         col_w = []
         col_b = []
@@ -393,7 +335,7 @@ class network:
     def training_procudure(self):
         # produce and X
         # while not the end of file
-        new_data = self.read_one_set_of_data(network.open_file_data_all)
+        new_data = self.read_one_set_of_data(Network.open_file_data)
         # print(new_data)
 
         while len(new_data[1]) != 0:
@@ -447,7 +389,7 @@ class network:
             # print(L1)
 
 
-            new_data = self.read_one_set_of_data(network.open_file_data_all)
+            new_data = self.read_one_set_of_data(Network.open_file_data)
 
         W = self.list_of_trained_w_and_b.get("w")
         B = self.list_of_trained_w_and_b.get("b")
@@ -468,34 +410,44 @@ class network:
             return "sell"
 
 
-# what the user can do
-
-
-num = 3
-
-list_of_layers = [10, 10, 2]
-input_data_count = 24
-error = 0.01
-
-file_hourly = "/Users/amir/Desktop/fxtime/cur_hours.csv"
-file_daily = "/Users/amir/Desktop/fxtime/cur_daily.csv"
-des_file = "/Users/amir/Desktop/fxtime/currency_new_version.csv"
-
-lst_of_new_data = [0.78751, 0.78761, 0.78754, 0.78743, 0.7873, 0.78741, 0.7871,
-                   0.78773, 0.78774, 0.78776, 0.78809, 0.78805, 0.78777, 0.78823, 0.78844, 0.78807,
-                   0.78814, 0.78814, 0.78817, 0.78834, 0.78824, 0.78821, 0.78825, 0.78833]
-
-#
-# file_hourly = "/Users/amir/Desktop/fxtime/curr_hours_mod.csv"
-# file_daily = "/Users/amir/Desktop/fxtime/curr_daily_mod.csv"
-# des_file = "/Users/amir/Desktop/fxtime/currency_new_version_mod.csv"
 
 
 
 
 
+            # what the user can do
 
-nn = network(error, num, list_of_layers, [file_daily, file_hourly, des_file], input_data_count)
-# it should print buy or sell
-nn.training_procudure()
-print(nn.predict(lst_of_new_data))
+
+if __name__ == "__main__":
+    num = 3
+
+    list_of_layers = [10, 10, 2]
+    input_data_count = 24
+    error = 0.01
+    rate_of_learning = 0.01
+
+    file_hourly = "/Users/amir/Desktop/fxtime/cur_hours.csv"
+    file_daily = "/Users/amir/Desktop/fxtime/cur_daily.csv"
+    # des_file = "/Users/amir/Desktop/fxtime/currency_new_version.csv"
+    data_file = "/Users/amir/Desktop/fxtime/currency_new_version.csv"
+
+    lst_of_new_data = [0.78751, 0.78761, 0.78754, 0.78743, 0.7873, 0.78741, 0.7871,
+                       0.78773, 0.78774, 0.78776, 0.78809, 0.78805, 0.78777, 0.78823, 0.78844, 0.78807,
+                       0.78814, 0.78814, 0.78817, 0.78834, 0.78824, 0.78821, 0.78825, 0.78833]
+
+    #
+    # file_hourly = "/Users/amir/Desktop/fxtime/curr_hours_mod.csv"
+    # file_daily = "/Users/amir/Desktop/fxtime/curr_daily_mod.csv"
+    # des_file = "/Users/amir/Desktop/fxtime/currency_new_version_mod.csv"
+
+
+    # provide the data from two files (file1 shows the daily candles info and file2 shows the hourly
+    # candles info)
+    neural_data.provide_data(file_daily, file_hourly, data_file)
+
+    print("I am here")
+
+    nn = Network(error, rate_of_learning, num, list_of_layers, data_file, input_data_count)
+    # it should print buy or sell
+    nn.training_procudure()
+    print(nn.predict(lst_of_new_data))
